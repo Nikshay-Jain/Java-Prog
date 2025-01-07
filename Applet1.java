3a3cbc23ddee7aac5574f0a7ba62d862c9846dde
Nikshay Jain <nikshay.p.jain@gmail.com>
2025-01-07 18:24:39 +0530

    Added Applet1.java

---
+++ Applet1.java
@@ -0,0 +1,9 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+public class Applet1 extends Applet
+{
+	public void paint(Graphics nik)
+	{
+		nik.drawString("Hello Nikshay!!!",300,300);
+	}
+}
