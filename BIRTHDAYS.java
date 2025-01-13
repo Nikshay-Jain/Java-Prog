719d67667d88e695188ad233644be976dbe9ba83
Nikshay Jain <nikshay.p.jain@gmail.com>
2025-01-13 16:40:11 +0530

    Added BIRTHDAYS.java

---
+++ BIRTHDAYS.java
@@ -0,0 +1,56 @@
+import java.util.*;
+class BIRTHDAYS
+{
+    public static void main (String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter your Birth date");
+        String b=s.nextLine();
+        int i;
+        switch (b)
+        {
+            case "19-04-2003":
+            for(i=1;i>=1;i++)
+            System.out.println("Happy Birthday Nikshay!!!");
+            break;
+            case "14-05-2007":
+            for(i=1;i>=1;i++)
+            System.out.println("Happy Birthday Darshiel!!!");
+            break;
+            case "13-12-1976":
+            for(i=1;i>=1;i++)
+            System.out.println("Happy Birthday Prashant!!!");
+            break;
+            case "8-12-1978":
+            for(i=1;i>=1;i++)
+            System.out.println("Happy Birthday Karishma!!!");
+            break;
+            case "19-12-2003":
+            for(i=1;i>=1;i++)
+            System.out.println("Happy Birthday Tarak!!!");
+            break;
+            case "06-04-2003":
+            for(i=1;i>=1;i++)
+            System.out.println("Happy Birthday Advait!!!");
+            break;
+            case "25-12-1976":
+            for(i=1;i>=1;i++)
+            System.out.println("Happy Birthday Sandeep!!!");
+            break;
+            case "23-01-2006":
+            for(i=1;i>=1;i++)
+            System.out.println("Happy Birthday Raksha!!!");
+            break;
+            case "02-11-2009":
+            for(i=1;i>=1;i++)
+            System.out.println("Happy Birthday Lakshya!!!");
+            break;
+            case "14-11-2006":
+            for(i=1;i>=1;i++)
+            System.out.println("Happy Birthday Mrigna!!!");
+            break;
+            default:
+            System.out.println("No Match Found");
+        }
+    }
+}
