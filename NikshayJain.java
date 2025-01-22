71cd04d515e989a3299f728483c08a62e072d6ae
Nikshay Jain <nikshay.p.jain@gmail.com>
2025-01-22 17:56:19 +0530

    Added NikshayJain.java

---
+++ NikshayJain.java
@@ -0,0 +1,16 @@
+class NikshayJain
+{
+    public static void main(String args[])
+    {
+        System.out.println("HI! MY NAME IS NIKSHAY JAIN.");
+        System.out.println("I LIKE SCIENCE VERY MUCH.");
+        System.out.println("I WAS BORN ON 19-04-2003 IN JODHPUR, RAJASTHAN, INDIA");
+        System.out.println("I LIKE JAVA PROGRAMMING.");
+        System.out.println();
+        System.out.println("*     *   *******");
+        System.out.println("* *   *      *");
+        System.out.println("*  *  *      *");
+        System.out.println("*   * *  *   *");
+        System.out.println("*     *   ***");
+    }
+}
