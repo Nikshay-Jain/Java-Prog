5a457eb3777cae0829f66c41fc21bd1d571e6dc5
Nikshay Jain <nikshay.p.jain@gmail.com>
2025-01-23 12:24:25 +0530

    Added Point1.java

---
+++ Point1.java
@@ -0,0 +1,12 @@
+class Point1
+{
+	public static void main(String args[])
+	{
+		Point p=new Point();
+		Point q=new Point();
+		p.setPoint(3,4);
+		p.print();
+		q.setPoint(30,40);
+		q.print();
+	}
+}
