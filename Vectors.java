dd91f87b26239198663b417586d5d749c5541393
Nikshay Jain <nikshay.p.jain@gmail.com>
2025-01-28 13:23:19 +0530

    Added Vectors.java

---
+++ Vectors.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Vectors
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter components of 1st vector");
+        int a[]=new int[3],i;
+        for(i=0;i<3;i++)
+        a[i]=s.nextInt();
+        System.out.println("Enter components of 2nd vector");
+        int b[]=new int[3];
+        for(i=0;i<3;i++)
+        b[i]=s.nextInt();
+        int m=0,n[]=new int[3];
+    }
+}
